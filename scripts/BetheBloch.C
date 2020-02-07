94e5d07da33f10492fe43c18e205c6d83009c5bd
KimuTsuri <ktsuri@hpx.phys.ocha.ac.jp>
2020-02-07 14:40:00 +0900

    chenge bethe bloch equation

---
+++ scripts/BetheBloch.C
@@ -0,0 +1,51 @@
+/*
+  betaDecay.C
+*/
+#include <iostream>
+#include <cmath>
+#include "TF1.h"
+#include "TH1.h"
+#include "TCanvas.h"
+
+Double_t func_BB(Double_t* x, Double_t* c) {
+  double bg = x[0];
+  double E = c[0];
+  double A = c[1];
+  double Z = c[2];
+
+  double K = 0.30701; // [MeVcm2/g]
+  double me = 0.511; // electron mass [MeV]
+
+  double b2 = bg*bg / (1 + bg*bg);
+  double g = bg / std::sqrt(b2);
+
+  double I = 16 * std::pow(Z, 0.9);
+  double ln = std::log( 2*me*b2*g*g /I);
+
+  double f = 0.0;
+  if (bg < 0) {
+    f = 0.0;
+  } else {
+    f = - (K*Z / (A*b2)) * (ln - b2) ;
+  }
+
+  return f;
+}
+
+void BetheBloch() {
+  TCanvas* c = new TCanvas("c", "", 0, 10, 600, 400);
+
+  TF1* f_BB = new TF1("Bethe Bloch;bg;Mass stopping power [$MeV cm^2/g$]", &func_BB, 0.001, 10.0, 3);
+  f_BB->SetParameter(0, 2.28);
+  f_BB->SetParameter(1, 28.0855); // A　[/mol]
+  f_BB->SetParameter(2, 14);  // Z [g/mol]
+  f_BB->SetNpx(100000);
+
+  //TH1* hframe=0;
+  //hframe = c->DrawFrame(0.0, 0.0, 2.5, 1.8);
+  //f_BB->Draw("same");
+  gPad->SetLogx();
+  gPad->SetLogy();
+  f_BB->Draw();
+
+}
--- scripts/betaDecay.C
+++ scripts/betaDecay.C
@@ -7,51 +7,72 @@
 #include "TH1.h"
 #include "TCanvas.h"
 
-Double_t func_decay(Double_t* x, Double_t *p) {
+Double_t func_decay(Double_t* x, Double_t *y) {
   // Unit: MeV
   double pe = x[0];
-  double N = p[0];
-  double E0 = p[1];
-  double m = p[2];
+  double E0 = y[0];
+  double N = 1.0 ; //coefficient
+  double me = 0.511; // electron mass [MeV]
 
-  double E = std::sqrt(pe*pe + m*m);
-  double de = E0 - E;
-  double y = 0.0;
+  double E = std::sqrt(pe*pe + me*me);
+  double dE = E0 - E;
+  double f = 0.0;
   if (pe < 0 || E > E0) {
-    y = 0.0;
+    f = 0.0;
   } else {
-    y = N*pe*E*de*de;
+    f = N*pe*E*dE*dE;
   }
-  return y;
+  return f;
+}
+/*
+Double_t func_BB(Double_t* beta, Double_t* x, Double_t* a, Double_t* z) {
+  double b = beta;
+  double dx = x;
+  double A = a;
+  double Z = z;
+  double K = 0.307075; // [MeVcm2/g]
+  double me = 0.511; // electron mass [MeV]
+
+  double I = 16 * std::pow(Z,0.9);
+  double g = 1/std::sqrt(1 - b*b); // gamma
+  double ln = std::log(2*me*b*g/I);
+  double f = K*z*z* Z * ln / (A*b*b)
 }
 
-Double_t func_absoption(Double_t* x, DOuble_t* p) {
+Double_t func_abs(Double_t* x, Double_t* y) {
   double pe = x[0];
-  double m = p[0];
-  double x = p[1]; // rho*l
+  double m = y[0];
+  double x = y[1]; // rho*l
+  double A = y[2];
+  double Z = y[3];
 
   double E = std::sqrt(pe*pe + m*m);
   double beta = pe/E;
 
-  //double dE = func_BB(beta, x);
-  double y=0;
-  if (dE > E) {
-    y = 1.0;
-  } else {
-    y = 0.0;
-  }
-  return y;
-}
+  double dE = func_BB(beta, x, A, Z);
 
+  double f=0.0;
+  if (dE > E) {
+    f = 1.0;
+  } else {
+    f = 0.0;
+  }
+  return f;
+}
+*/
 void betaDecay() {
+  TCanvas* c = new TCanvas("c", "", 0, 0, 500, 500);
+
   TF1* f_decay = new TF1("f_decay", &func_decay, 0.0, 5.0, 3);
-  f_decay->SetParameter(0, 1.0);
-  f_decay->SetParameter(1, 2.28);
-  f_decay->SetParameter(2, 0.511);
+  f_decay->SetParameter(0, 2.28);
   f_decay->SetNpx(10000);
 
-  TCanvas* c = new TCanvas("c", "", 0, 0, 500, 500);
   TH1* hframe=0;
   hframe = c->DrawFrame(0.0, 0.0, 2.5, 1.8);
   f_decay->Draw("same");
+
+
+
+
+
 }
